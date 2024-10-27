0ac8e9c89bdd9325e703d2e366c2f2e74b1383e3
narminmusayeva <narminmsyva@gmail.com>
2024-10-27 16:12:20 +0000

    done

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /*
- * main - prints all single digit numbers of base 10 starting from 0
+ * main - prints numbers from 0 to 9
  * return : always 0 (success)
  */
 int main(void)
 {
 char c;
-for (c = 0; c <= 10; c++)
+for (c = 0; c <= 9; c++)
 putchar(c);
 putchar('\n');
 return (0);
