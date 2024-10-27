898b9c71ecdc9e3d41de32f59a1d9254c475cfdb
narminmusayeva <narminmsyva@gmail.com>
2024-10-27 16:38:07 +0000

    done

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints rhe lowercase alphabet in reverse
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c <= 'a'; c--)
+putchar(c);
+putchar('\n');
+return (0);
+}
