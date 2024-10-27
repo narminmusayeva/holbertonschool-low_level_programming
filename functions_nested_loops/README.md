67da2b5c89c65980f05b2c3ff62415cc47fcd996
narminmusayeva <narminmsyva@gmail.com>
2024-10-27 17:38:12 +0000

    done

---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this project about C- functions,nested loops
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void print_numbers(void);
+void print_alphabet(void);
+void print_comb(void);
+#endif /* MAIN_H */
+
