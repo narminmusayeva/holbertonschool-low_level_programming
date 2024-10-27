c09d8543f064f31136530f339956a0aee41e9c79
narminmusayeva <narminmsyva@gmail.com>
2024-10-27 16:27:54 +0000

    done

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /*
  * main - Entry point of the program
- * Description: Print numbers from 9 to 0
+ * Description: Print numbers from 0 to 9 using puthcar
  * return: always 0 (success)
  */
 int main(void)
 {
 int c;
-for(c = '0'; c <= '9'; c++)
+for (c = '0'; c <= '9'; c++)
 putchar(c);
 putchar('\n');
 return (0);
