9451a4741b544b9460d50fada5fd8e1be061639b
narminmusayeva <narminmsyva@gmail.com>
2024-10-28 15:59:04 +0000

    done

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * _isdigit - check digits
+ * @c :checker simvol
  * Return: if digit 1,else 0
  */
 int _isdigit(int c)
 {
-if (c <= '0' && c >= '9')
+if (c >= '0' && c <= '9')
 return (1);
 else
 return (0);
