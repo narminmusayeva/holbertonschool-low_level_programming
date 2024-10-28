5a710066636adf88c73e85d9833da5d4e9393b79
narminmusayeva <narminmsyva@gmail.com>
2024-10-28 15:43:45 +0000

    done

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - check uppercase
+ * @c: checker simvol
+ * Return: if uppercase 1, else 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+/* main.h*/
+#ifndef MAIN_H
+#define MAIN_H
+int _isupper(int c);
+#endif /* MAIN_H */
