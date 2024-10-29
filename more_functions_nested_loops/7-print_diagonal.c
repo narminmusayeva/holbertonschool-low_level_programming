b1cc12c719c23b98a0d5dd52f8b7830c780967e5
narminmusayeva <narminmsyva@gmail.com>
2024-10-29 08:29:35 +0000

    done

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,21 +6,21 @@
  */
 void print_diagonal(int n)
 {
+int i;
+int j;
 if (n <= 0)
 {
 _putchar ('\n');
 return;
 }
-else
+for (i = 0; i < n; i++)
 {
-_putchar ('\\');
-_putchar ('\n');
+for (j = 0; j < i; j++)
+{
 _putchar (' ');
-_putchar ('\\');
-_putchar ('\n');
-_putchar (' ');
-_putchar (' ');
+}
 _putchar ('\\');
 _putchar ('\n');
 }
+_putchar ('\n');
 }
