dfaea2e9aa9d3dfc112e8e31330de52604b892b0
narminmusayeva <narminmsyva@gmail.com>
2024-10-29 05:57:38 +0000

    done

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,11 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-if (j > 9)
+if (j / 10)
 {
-_putchar('1');
-j %= 10;
+_putchar('0' + (j / 10));
 }
-else
-{
-_putchar(j + '0');
-}
+_putchar((j % 10) + '0');
 }
 _putchar('\n');
 }
