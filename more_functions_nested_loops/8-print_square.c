17ec548874518e6ce545fb72e707d05855517f09
narminmusayeva <narminmsyva@gmail.com>
2024-10-30 12:56:22 +0000

    done

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,20 +2,22 @@
 /**
  * print_square - print # simvol
  * _putchar: print square simvol
- * @size:The number of underscores to be printed.
+ * @size:size of the square
  */
 void print_square(int size)
 {
 int i, j;
 if (size <= 0)
+{
 _putchar ('\n');
 return;
+}
 for (i = 0; i < size; i++)
 {
-_putchar ('#');
 for (j = i; j < i; j++)
+{
 _putchar ('#');
-_putchar ('\n');
+}
 }
 _putchar ('\n');
 }
