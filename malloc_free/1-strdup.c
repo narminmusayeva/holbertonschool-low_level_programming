ed2b17a0b4e9254f4e8e54ba7753e20f3d15aa7e
narminmusayeva <narminmsyva@gmail.com>
2024-11-20 08:47:47 +0000

    done

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,20 +9,15 @@
 char *_strdup(char *str)
 {
 char *arr;
-int i;
 if (str == NULL)
 {
 return (NULL);
 }
-arr = (char *)malloc(sizeof(char) * (strlen(str) + 1));
+arr = (char *)malloc(strlen(str) + 1);
 if (arr == NULL)
 {
 return (NULL);
 }
-for (i = 0; str[i] != '\0'; i++)
-{
-arr[i] = str[i];
-}
-arr[i] = '\0';
+strcpy(arr, str);
 return (arr);
 }
