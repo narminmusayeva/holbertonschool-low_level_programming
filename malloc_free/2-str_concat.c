532d0aa82226c2f9f08c3645901db7cca104a41c
narminmusayeva <narminmsyva@gmail.com>
2024-11-22 06:38:40 +0000

    done

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,32 +1,30 @@
 #include "main.h"
+#include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
 /**
- * str_concat - Concatenates two strings.
- * @s1: First string.
- * @s2: Second string.
- *
- * Return: New concatenated string or NULL.
- */
+* str_concat - a function that concatenates two strings.
+*@s1:First string
+*@s2:Second string
+*
+*Return: NULL in case of failure , but pointer to new string in
+*case of success
+*/
 char *str_concat(char *s1, char *s2)
 {
-char *arr;
-int i, j;
+char *concat_str;
+int index, concat_index = 0,  len = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-arr = (char *)malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
-if (arr == NULL)
+for (index = 0; s1[index] || s2[index]; index++)
+len++;
+concat_str = malloc(sizeof(char) * len);
+if (concat_str == NULL)
 return (NULL);
-for (i = 0; s1[i] != '\0'; i++)
-{
-arr[i] = s1[i];
-}
-for (j = 0; s2[j] != '\0'; j++)
-{
-arr[i + j] = s2[j];
-}
-arr[i + j] = '\0';
-return (arr);
+for (index = 0; s1[index]; index++)
+concat_str[concat_index++] = s1[index];
+for (index = 0; s2[index]; index++)
+concat_str[concat_index++] = s2[index];
+return (concat_str);
 }
