2dd8c548303ebecf407b17da7e4ba3a9e314af6e
narminmusayeva <narminmsyva@gmail.com>
2024-12-08 19:44:33 +0000

    done

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,7 +4,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
 /**
  * main - entry point
  * @ac: argument count
@@ -19,38 +18,28 @@ int main(int ac, char *av[])
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-
-	
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-
 	rd_stat = 1;
-	while (rd_stat > 0)
+	while (rd_stat)
 	{
 		rd_stat = read(fd_from, buffer, 1024);
 		if (rd_stat == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-
 		if (rd_stat > 0)
 		{
 			wr_stat = write(fd_to, buffer, rd_stat);
-			if (wr_stat == -1 || wr_stat != rd_stat)
+			if (wr_stat != rd_stat || wr_stat == -1)
 				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 		}
 	}
-
-	
 	if (close(fd_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-
 	if (close(fd_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-
 	return (0);
 }
-
