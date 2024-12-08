632f48aa2edadb264f3911c7954fdb4a74d63e57
narminmusayeva <narminmsyva@gmail.com>
2024-12-08 19:41:28 +0000

    done

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
 /**
  * main - entry point
  * @ac: argument count
@@ -18,28 +19,39 @@ int main(int ac, char *av[])
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	// File_from açılır
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+	// File_to açılır (əgər varsa sıfırlanır)
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+
 	rd_stat = 1;
-	while (rd_stat)
+	while (rd_stat > 0)
 	{
 		rd_stat = read(fd_from, buffer, 1024);
 		if (rd_stat == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
 		if (rd_stat > 0)
 		{
 			wr_stat = write(fd_to, buffer, rd_stat);
-			if (wr_stat != rd_stat || wr_stat == -1)
+			if (wr_stat == -1 || wr_stat != rd_stat)
 				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 		}
 	}
+
+	// Faylları bağlama
 	if (close(fd_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+
 	if (close(fd_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+
 	return (0);
 }
+
